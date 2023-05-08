ab699184d83e6271e7dc7e526a7f1d04a0773883
MikoloD <grygiel.mikolaj@gmail.com>
2023-05-08 18:49:08 +0200

    style changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,6 @@ function App() {
             <header className={styles['App-header']}>
                 <img src={logo} className={styles['App-logo']} alt="logo" />
                 <SearchBar />
-                <select>
-                    <option>Apple</option>
-                    <option>Banana</option>
-                    <option>Watermelon</option>
-                </select>
                 <a
                     className={styles['App-link']}
                     href="https://reactjs.org"
