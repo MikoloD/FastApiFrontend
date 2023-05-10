63879a46e0f735e21b4a50275c1a966b0bef693b
MikoloD <grygiel.mikolaj@gmail.com>
2023-05-10 15:53:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import logo from './assets/logo.svg';
 import styles from './App.module.scss';
 import SearchBar from './components/SearchBar'
 import Recomendation from './components/Recomendation';
+import { useState } from 'react';
 
 function App() {
     return (
-
                 <div className={styles.App}>
                     <header className={styles['App-header']}>
                         <img src={logo} className={styles['App-logo']} alt="logo" />
